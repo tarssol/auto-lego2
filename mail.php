301ec3e8d21e35c928c9776670f9ee037abb3433
tarssol <66141650+tarssol@users.noreply.github.com>
2021-07-07 10:55:55 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -17,7 +17,7 @@ $mail->SMTPAuth = true;                               // Enable SMTP authenticat
 $mail->Username = 'tarassolyarevich@gmail.com'; // Ваш логин от почты с которой будут отправляться письма
 $mail->Password = 'immilliarder'; // Ваш пароль от почты с которой будут отправляться письма
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->Port = 587; // TCP port to connect to / этот порт может отличаться у других провайдеров
 
 $mail->setFrom('tarassolyarevich@gmail.com'); // от кого будет уходить письмо?
 $mail->addAddress('tarassolyarevich@gmail.com');     // Кому будет уходить письмо 
